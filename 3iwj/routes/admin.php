08e0980ddd0db33926de56f79f106b00902beec6
Hoang Nam Nguyen <77730298+asil-x@users.noreply.github.com>
2025-01-03 18:43:01 +0100

    Delete 3iwj directory

--- 3iwj/routes/admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware(['role:admin'])->group(function () {
-    Route::resource('users', UserController::class)->except(['create', 'show']);
-});
--- 3iwj/routes/tag.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\TagController;
-use Illuminate\Support\Facades\Route;
-
-Route::resource('tags', TagController::class)
-    ->except(['show'])
-    ->middleware(['auth']);
